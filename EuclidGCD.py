41eff440d3792b6177c53f9a7972a64dd3255381
Nicole Ignasiak <nicole.ignasiak@myemail.indwes.edu>
2015-12-04 19:24:56 +0000

    Last Week Assignment

--- EuclidGCD.py
+++ EuclidGCD.py
@@ -1,4 +1,18 @@
+#Nicole Ignasiak
+#CIS 125 
+#Dec 4, 2015
+
 def gCd(x, y):
-    if ???# base case here
-        return ???
-    return gCd(?????)
+    print("X = ", x, "Y = ", y)
+    if y == 0: # base case here
+        return x
+    else:
+        return gCd(y, x%y)
+        
+def main():
+    x = 1074
+    y = 52
+    print("GCD of ", x, ", ", y, " = ", gCd(x,y))
+    
+if __name__ == "__main__":
+    main()
